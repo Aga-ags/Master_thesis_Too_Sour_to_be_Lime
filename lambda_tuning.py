d3d47a5fc73d9b677b970602f23511b0a2beee99
Aga-ags <aga.agnieszka.kubica@gmail.com>
2025-06-24 08:12:02 +0200

    stuff

--- lambda_tuning.py
+++ lambda_tuning.py
@@ -29,61 +29,76 @@ results_validation = train.load_model_and_predict(model_name = "both_no_lime_imp
 # size lambda of the penaltly term
 
 results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
-                                    model_name = "both_no_3_5_classes_dropout_penatly_1",
+                                    model_name = "both_no_3_5_classes_penatly_1",
                                     list_of_outputs = ["pH", "lime"],
-                                    loss_function = {'pH': 'mse', 'lime': train.zero_inflated_mse_loss},
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
                                     results_validation=results_validation,
                                     results_test=results_test,
                                     include_weigths = True, 
-                                    include_dropout = True, 
-                                    include_lime_ph_penalty = False, 
+                                    include_lime_ph_penalty = True, 
                                     lambda_penalty = 1)
 
 
-
 results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
-                                    model_name = "both_no_3_5_classes_dropout_penatly_05",
+                                    model_name = "both_no_3_5_classes_penatly_05",
                                     list_of_outputs = ["pH", "lime"],
-                                    loss_function = {'pH': 'mse', 'lime': train.zero_inflated_mse_loss},
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
                                     results_validation=results_validation,
                                     results_test=results_test,
                                     include_weigths = True, 
-                                    include_dropout = True, 
-                                    include_lime_ph_penalty = False, 
+                                    include_lime_ph_penalty = True, 
                                     lambda_penalty = 0.5)
 
 
 results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
-                                    model_name = "both_no_3_5_classes_dropout_penatly_01",
+                                    model_name = "both_no_3_5_classes_penatly_01",
                                     list_of_outputs = ["pH", "lime"],
-                                    loss_function = {'pH': 'mse', 'lime': train.zero_inflated_mse_loss},
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
                                     results_validation=results_validation,
                                     results_test=results_test,
                                     include_weigths = True, 
-                                    include_dropout = True, 
-                                    include_lime_ph_penalty = False, 
+                                    include_lime_ph_penalty = True, 
                                     lambda_penalty = 0.1)
 
 results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
-                                    model_name = "both_no_3_5_classes_dropout_penatly_001",
+                                    model_name = "both_no_3_5_classes_penatly_005",
                                     list_of_outputs = ["pH", "lime"],
-                                    loss_function = {'pH': 'mse', 'lime': train.zero_inflated_mse_loss},
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
+                                    results_validation=results_validation,
+                                    results_test=results_test,
+                                    include_weigths = True,  
+                                    include_lime_ph_penalty = True, 
+                                    lambda_penalty = 0.05)
+
+results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
+                                    model_name = "both_no_3_5_classes_penatly_001",
+                                    list_of_outputs = ["pH", "lime"],
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
                                     results_validation=results_validation,
                                     results_test=results_test,
                                     include_weigths = True, 
-                                    include_dropout = True, 
-                                    include_lime_ph_penalty = False, 
+                                    include_lime_ph_penalty = True, 
                                     lambda_penalty = 0.01)
 
 results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
-                                    model_name = "both_no_3_5_classes_dropout_penatly_0001",
+                                    model_name = "both_no_3_5_classes_penatly_0005",
                                     list_of_outputs = ["pH", "lime"],
-                                    loss_function = {'pH': 'mse', 'lime': train.zero_inflated_mse_loss},
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
+                                    results_validation=results_validation,
+                                    results_test=results_test,
+                                    include_weigths = True,
+                                    include_lime_ph_penalty = True, 
+                                    lambda_penalty = 0.005)
+
+
+results_validation, results_test = train.perform_moodel_training_with_tuning(imputation_scenario = "no_lime_imputation_from_3_5_classes", 
+                                    model_name = "both_no_3_5_classes_penatly_0001",
+                                    list_of_outputs = ["pH", "lime"],
+                                    loss_function = {'pH': 'mse', 'lime': 'mse'},
                                     results_validation=results_validation,
                                     results_test=results_test,
                                     include_weigths = True, 
-                                    include_dropout = True, 
-                                    include_lime_ph_penalty = False, 
+                                    include_lime_ph_penalty = True, 
                                     lambda_penalty = 0.001)
 
 
